82e014fb9bc19f49e2524fb46e49ddfd4dd4d35b
nsahak <noorr.sahak@gmail.com>
2020-12-29 18:20:49 -0800

    This is new changes

---
+++ src/car_dealership/NewPracticeClass_01.java
@@ -0,0 +1,12 @@
+package car_dealership;
+
+public class NewPracticeClass_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the new added class; for practice");
+
+	}
+
+}
